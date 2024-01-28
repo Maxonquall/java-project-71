7b2a03a6f50912b4cde3b77528e8b7bdf6a269d0
Maxim <msirotkin95@gmail.com>
2024-01-28 13:47:41 +0300

    Step 3

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,45 @@
 package hexlet.code;
 
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+import java.io.File;
+import java.math.BigInteger;
+import java.nio.file.Files;
+import java.security.MessageDigest;
+import java.util.concurrent.Callable;
+
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
+        description = "Compares two configuration files and shows a difference.")
+class App implements Callable<Integer> {
+
+  /*  @Parameters(description = "path to 1st file.")
+    private File filepath1;
+
+    @Parameters(description = "path to 2nd file.")
+    private File filepath2;*/
+
+  /*  @Option(names = {"-f", "--format"}, description = "output format [default: stylish]")
+    private String format = "format";*/
+
+    @Override
+    public Integer call() throws Exception { // your business logic goes here...
+       /* byte[] fileContents1 = Files.readAllBytes(filepath1.toPath());
+        byte[] fileContents2 = Files.readAllBytes(filepath2.toPath());
+        byte[] digest1 = MessageDigest.getInstance(format).digest(fileContents1);
+        byte[] digest2 = MessageDigest.getInstance(format).digest(fileContents2);
+        System.out.printf("%0" + (digest1.length*2) + "x%n", new BigInteger(1, digest1));
+        System.out.printf("%0" + (digest2.length*2) + "x%n", new BigInteger(1, digest2));*/
+        return 0;
     }
+
+
+    public static void main(String... args) {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
+    }
+
+
 }
