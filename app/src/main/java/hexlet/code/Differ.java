908c545b7df2007fcd54f2b16c2e96a2f373fdc9
Maxim <msirotkin95@gmail.com>
2024-02-03 14:41:11 +0300

    Step 5

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,53 +1,24 @@
 package hexlet.code;
 
-import java.util.TreeMap;
+import java.util.*;
 import java.util.stream.Collectors;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Set;
-import java.util.TreeSet;
-import java.util.ArrayList;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class Differ {
     public static  String generate(String filepath1, String filepath2) throws Exception {
 
-      //  String filepath1 = "file1.json";
-      //  String filepath2 = "file2.json";
-        var file1 = Paths.get(filepath1).toAbsolutePath().normalize().toString();
-        var file2 = Paths.get(filepath2).toAbsolutePath().normalize().toString();
-
-        var map1 = Arrays.stream(file1.split(",\n"))
-                .map(str -> str.split(": "))
-                .collect(Collectors.toMap(
-                        str -> str[0].replaceAll("\"", ""),
-                        str -> {
-                            if (str[1].equals("true") || str[1].equals("false")) {
-                                return Boolean.parseBoolean(str[1]);
-                            } else if (str[1].startsWith("\"")) {
-                                return str[1].replaceAll("\"", "");
-                            } else {
-                                return Integer.parseInt(str[1]);
-                            }
-                        }
-                ));
-
-        var map2 = Arrays.stream(file2.split(",\n"))
-                .map(str -> str.split(": "))
-                .collect(Collectors.toMap(
-                        str -> str[0].replaceAll("\"", ""),
-                        str -> {
-                            if (str[1].equals("true") || str[1].equals("false")) {
-                                return Boolean.parseBoolean(str[1]);
-                            } else if (str[1].startsWith("\"")) {
-                                return str[1].replaceAll("\"", "");
-                            } else {
-                                return Integer.parseInt(str[1]);
-                            }
-                        }
-                ));
+        var file1 = Paths.get(filepath1).toAbsolutePath().normalize();
+        var file2 = Paths.get(filepath2).toAbsolutePath().normalize();
+        var file1Str = Files.readString(file1);
+        var file2Str = Files.readString(file2);
+        ObjectMapper objectMapper = new ObjectMapper();
+        var map1 = objectMapper.readValue(file1Str, new TypeReference<Map<String,Object>>(){});
+        var map2 = objectMapper.readValue(file2Str, new TypeReference<Map<String,Object>>(){});
 
         var result = new ArrayList<String>();
 
@@ -56,13 +27,13 @@ public class Differ {
         for (String key : keys) {
             if (map1.containsKey(key) && map2.containsKey(key)) {
                 if (map1.get(key).equals(map2.get(key))) {
-                    result.add(key + ": " + map1.get(key));
+                    result.add("  " + key + ": " + map1.get(key));
                 } else {
-                    result.add("-" + key + ": " + map1.get(key));
-                    result.add("+" + key + ": " + map2.get(key));
+                    result.add("- " + key + ": " + map1.get(key));
+                    result.add("+ " + key + ": " + map2.get(key));
                 }
             } else if (map1.containsKey(key)) {
-                result.add("-" + key + ": " + map1.get(key));
+                result.add("- " + key + ": " + map1.get(key));
             } else {
                 result.add("+ " + key + ": " + map2.get(key));
             }
