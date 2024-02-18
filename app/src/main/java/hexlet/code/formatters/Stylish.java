657da738beac57268687e7904441393b70ecf03b
Maxim <msirotkin95@gmail.com>
2024-02-18 18:22:34 +0300

    Autocheck

--- app/src/main/java/hexlet/code/formatters/Stylish.java
+++ app/src/main/java/hexlet/code/formatters/Stylish.java
@@ -9,18 +9,18 @@ public class Stylish {
 
         for (var valueMap : parsedList) {
             if (valueMap.get("diff_value").equals("added")) {
-                resultToString.append("+ " + valueMap.get("master_key") + ": "
+                resultToString.append("  + " + valueMap.get("master_key") + ": "
                         + valueMap.get("new_value")).append("\n");
             } else if (valueMap.get("diff_value").equals("deleted")) {
-                resultToString.append("- " + valueMap.get("master_key") + ": "
+                resultToString.append("  - " + valueMap.get("master_key") + ": "
                         + valueMap.get("old_value")).append("\n");
             } else if (valueMap.get("diff_value").equals("unchanged")) {
-                resultToString.append("  " + valueMap.get("master_key") + ": "
+                resultToString.append("    " + valueMap.get("master_key") + ": "
                         + valueMap.get("old_value")).append("\n");
             } else {
-                resultToString.append("- " + valueMap.get("master_key") + ": "
+                resultToString.append("  - " + valueMap.get("master_key") + ": "
                         + valueMap.get("old_value")).append("\n");
-                resultToString.append("+ " + valueMap.get("master_key") + ": "
+                resultToString.append("  + " + valueMap.get("master_key") + ": "
                         + valueMap.get("new_value")).append("\n");
             }
         }
