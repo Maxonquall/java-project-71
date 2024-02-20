bf6109c1e97b33ab897124969a3bfe5ac7b2e0f1
Maxim <msirotkin95@gmail.com>
2024-02-20 20:56:02 +0300

    Autocheck

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -12,17 +12,15 @@ import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 public class Differ {
     public static String generate(String filepath1, String filepath2, String style) throws Exception {
         var diff = "";
-      //  var dataFormat = "";
         var file1 = Paths.get(filepath1).toAbsolutePath().normalize();
         var file2 = Paths.get(filepath2).toAbsolutePath().normalize();
         var file1Str = Files.readString(file1);
         var file2Str = Files.readString(file2);
-      //  var dataFormat = file1Str.lastIndexOf(".");
-        int index = file1Str.lastIndexOf('.');
-        var dataFormat = index > 0 ?  file1Str.substring(index + 1) : "";
-      /*  return index > 0
-                ?  var String dataFormat = file1Str.substring(index + 1)
-                : "";*/
+        //var dataFormat = file1Str.split(".")[1];
+        int index = filepath1.lastIndexOf('.');
+        var dataFormat = index > 0
+                ? filepath1.substring(index + 1)
+                : "";
 
         switch (dataFormat) {
             case "yml" -> {
@@ -31,12 +29,13 @@ public class Differ {
                 var map2 = mapper.readValue(file2Str, new TypeReference<Map<String, Object>>() { });
                 diff = Parser.compare(map1, map2, style);
             }
-            default -> {
+            case "json " -> {
                 ObjectMapper objectMapper = new ObjectMapper();
                 var map1 = objectMapper.readValue(file1Str, new TypeReference<Map<String, Object>>() { });
                 var map2 = objectMapper.readValue(file2Str, new TypeReference<Map<String, Object>>() { });
                 diff = Parser.compare(map1, map2, style);
             }
+            default -> throw new IllegalArgumentException("Wrong format");
         }
 
         return diff;
