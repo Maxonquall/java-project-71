6de26e08e22b4b668fd7f987a0d80db6a01283e4
Maxim <msirotkin95@gmail.com>
2024-02-26 14:47:09 +0300

    Check 1

--- app/src/test/java/hexlet/code/ApplicationTest.java
+++ app/src/test/java/hexlet/code/ApplicationTest.java
@@ -15,57 +15,58 @@ class ApplicationTest {
     private static String readFile(String fileName) throws IOException {
         return Files.readString(Paths.get(fileName).toAbsolutePath().normalize()).trim();
     }
+    final static String prefix = "src/test/resources/fixtures/";
 
     @Test
     void jsonDefaultTest() throws Exception  {
         String expected = readFile("src/test/resources/fixtures/Stylish");
-        String actual = Differ.generate("file1.json", "file2.json");
+        String actual = Differ.generate(prefix + "file1.json", prefix + "file2.json");
         assertEquals(expected, actual);
     }
     @Test
     void ymlDefaultTest() throws Exception  {
         String expected = readFile("src/test/resources/fixtures/Stylish");
-        String actual = Differ.generate("file1.yml", "file2.yml");
+        String actual = Differ.generate(prefix + "file1.yml", prefix + "file2.yml");
         assertEquals(expected, actual);
     }
     @Test
         void jsonStylishTest() throws Exception  {
         String expected = readFile("src/test/resources/fixtures/Stylish");
-        String actual = Differ.generate("file1.json", "file2.json", "stylish");
+        String actual = Differ.generate(prefix + "file1.json", prefix + "file2.json", "stylish");
         assertEquals(expected, actual);
     }
     @Test
     void ymlStylishTest() throws Exception  {
         String expected = readFile("src/test/resources/fixtures/Stylish");
-        String actual = Differ.generate("file1.yml", "file2.yml", "stylish");
+        String actual = Differ.generate(prefix + "file1.yml", prefix + "file2.yml", "stylish");
         assertEquals(expected, actual);
     }
 
     @Test
     void jsonPlainTest() throws Exception  {
         String expected = readFile("src/test/resources/fixtures/Plain");
-        String actual = Differ.generate("file1.json", "file2.json", "plain");
+        String actual = Differ.generate(prefix + "file1.json", prefix + "file2.json", "plain");
         assertEquals(expected, actual);
     }
 
     @Test
     void ymlPlainTest() throws Exception  {
         String expected = readFile("src/test/resources/fixtures/Plain");
-        String actual = Differ.generate("file1.yml", "file2.yml", "plain");
+        String actual = Differ.generate(prefix + "file1.yml", prefix + "file2.yml", "plain");
         assertEquals(expected, actual);
     }
 
     @Test
     void jsonJsonTest() throws Exception  {
         String expected = readFile("src/test/resources/fixtures/JSON");
-        String actual = Differ.generate("file1.json", "file2.json", "json");
+        String actual = Differ.generate(prefix + "file1.json", prefix + "file2.json", "json");
         assertEquals(expected, actual);
     }
 
     @Test
     void ymlJsonTest() throws Exception  {
         String expected = readFile("src/test/resources/fixtures/JSON");
-        String actual = Differ.generate("file1.yml", "file2.yml", "json");
+        String actual = Differ.generate(prefix + "file1.yml", prefix + "file2.yml", "json");
         assertEquals(expected, actual);
     }
 
